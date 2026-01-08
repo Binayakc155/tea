5a1fbfa4bcb842bb8efa7c8a75cc0069b3da24e0
binaya kc <127301133+Binayakc155@users.noreply.github.com>
2026-01-08 23:30:04 +0545

    while user click in the logout then then its redirect the main pages

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -7,7 +7,7 @@ export async function POST() {
   await supabase.auth.signOut(); // ✅ works now
   return NextResponse.redirect(
     new URL(
-      '/admin/login',
+      '/',
       process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
     )
   );
